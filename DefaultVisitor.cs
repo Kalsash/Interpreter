70b0d73a2ecf50768ec2d4574b0eed2469de65c9
Александр Калинин <kalsash@yandex.ru>
2022-10-12 12:18:00 +0300

    Разделение переменных по типам

--- DefaultVisitor.cs
+++ DefaultVisitor.cs
@@ -5,84 +5,134 @@ using System.Text;
 using System.Xml.Linq;
 using ProgramTree;
 using SimpleLang.Visitors;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace SimpleLang
 {
     public enum type { tint, tdouble };
+    struct Var
+    {
+        public type type;
+        public string value;
+    }
     class DefaultVisitor: Visitor
     {
-        public Dictionary<string, double> Vars = new Dictionary<string, double>(); // таблица символов
+        public Dictionary<string, Var> Vars = new Dictionary<string, Var>(); // таблица символов
         public double Loop_Counter = -1;
-        public double Value;
-        public void NewVarDef(string name, double val)
+        public double dValue;
+        public int iValue;
+        public type Type;
+        public void NewVarDef(string name, Var v)
         {
             if (Vars.ContainsKey(name))
-                Vars[name] = val;
-            else Vars.Add(name, val);
+                Vars[name] = v;
+            else Vars.Add(name, v);
         }
         public override void VisitIdNode(IdNode id)
         {
             if (Vars.ContainsKey(id.Name))
             {
-                Value = Vars[id.Name];
+                if (Vars[id.Name].type == type.tint)
+                {
+                    iValue = Int32.Parse(Vars[id.Name].value);
+                    Type = type.tint;
+                }
+                if (Vars[id.Name].type == type.tdouble)
+                {
+                    dValue = double.Parse(Vars[id.Name].value);
+                    Type = type.tdouble;
+                }
+
             }
         }
         public override void VisitIntNumNode(IntNumNode num) 
         {
-            Value = num.Num;
+            iValue = num.Num;
+            Type = type.tint;
         }
         public override void VisitRealNumNode(RealNumNode num)
         {
-            Value = num.Num;
+            dValue = num.Num;
+            Type = type.tdouble;
         }
 
         public override void VisitBinOpNode(BinOpNode binop)
         {
+           // type t1 = type.tdouble;
+            //type t2 = type.tdouble;
             binop.Left.Eval(this);
-            double val1 = Value;
+            //if (Type == type.tint)
+            //{
+            //    t1 = type.tint;
+            //    int val1 = iValue;
+            //}
+            //else
+            //if (Type == type.tdouble)
+            //{
+            //    double val1 = iValue;
+            //}
             binop.Right.Eval(this);
-            double val2 = Value;
-            switch (binop.Op)
-            {
+            //if (Type == type.tint)
+            //{
+            //    int val2 = iValue;
+            //}
+            //else
+            //if (Type == type.tdouble)
+            //{
+            //    double val2 = iValue;
+            //}
+            //switch (binop.Op)
+            //{
            
-                case '+':
-                    Value = val1 + val2;
-                    break;
-                case '-':
-                    Value = val1 + val2;
-                    break;
-                case '*':
-                    Value = val1 + val2;
-                    break;
-                case '/':
-                    Value = val1 + val2;
-                    break;
-            }
+            //    case '+':
+            //        dValue = val1 + val2;
+            //        break;
+            //    case '-':
+            //        Value = val1 - val2;
+            //        break;
+            //    case '*':
+            //        Value = val1 * val2;
+            //        break;
+            //    case '/':
+            //        Value = val1 / val2;
+            //        break;
+            //}
         }
         public override void VisitAssignNode(AssignNode a)
         {
             // для каких-то визиторов порядок может быть обратный - вначале обойти выражение, потом - идентификатор
             a.Id.Eval(this);
             a.Expr.Eval(this);
-            NewVarDef(a.Id.Name, Value);
+            Var v = new Var();
+            if (Type == type.tint)
+            {
+                v.type = type.tint;
+                v.value = iValue.ToString();
+            }
+            if (Type == type.tdouble)
+            {
+                v.type = type.tdouble;
+                v.value = dValue.ToString();
+            }
+            NewVarDef(a.Id.Name, v);
         }
         public override void VisitLoopNode(LoopNode l)
         {
-            l.Expr.Eval(this);
-            {
+            //l.Expr.Eval(this);
+            //{
 
-            }
-            if (Loop_Counter == -1)
-            {
-                Loop_Counter = Value;
-            }
-            if (Loop_Counter > 0)
-            {
-                Loop_Counter--;
-                l.Stat.Eval(this);
-                VisitLoopNode(l);
-            }
-            Loop_Counter = -1;
+            //}
+            //if (Loop_Counter == -1)
+            //{
+            //    Loop_Counter = Value;
+            //}
+            //if (Loop_Counter > 0)
+            //{
+            //    Loop_Counter--;
+            //    l.Stat.Eval(this);
+            //    VisitLoopNode(l);
+            //}
+            //Loop_Counter = -1;
         }
         public override void VisitWhileNode(WhileNode w)
         {
@@ -111,8 +161,16 @@ namespace SimpleLang
         }
         public override void VisitWriteNode(WriteNode w)
         {
-            w.Expr.Eval(this); 
-            Console.WriteLine(Value);
+            w.Expr.Eval(this);
+            if (Type == type.tint)
+            {
+                Console.WriteLine("Переменная со значением " + iValue + " типа int");
+            }
+            if (Type == type.tdouble)
+            {
+                Console.WriteLine("Переменная со значением " + dValue + " типа double");
+            }
+
         }
     }
 }
